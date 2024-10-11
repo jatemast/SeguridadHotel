4b57632a747a3f66e9fa48930f314782dffaf2de
javier teheran <programador14@tecnologicocomfenalco.edu.co>
2024-10-11 14:18:13 -0500

    ya se le agrego el sider  ala aplicacion

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,11 +4,14 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    <x-sider />
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+                 <h1>dasboard</h1>
+
+                 
             </div>
         </div>
     </div>
