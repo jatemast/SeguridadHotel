3fdf78f65344e296d83c2bee9657ac26c6face1b
javier teheran <programador14@tecnologicocomfenalco.edu.co>
2024-10-18 16:46:22 -0500

    se le agrego el efecto alos componentes

--- resources/views/mantenimientos/index.blade.php
+++ resources/views/mantenimientos/index.blade.php
@@ -3,7 +3,6 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Módulo de Mantenimiento') }}
 
-
             <h1>Aquí encontrarás los materiales que se usan en esta área y los riesgos que estos tienen para los trabajadores</h1>
         </h2>
     </x-slot>
@@ -11,20 +10,34 @@
 
     <div class="flex flex-col items-center">
         <div class="flex justify-center w-full mb-4">
-            <x-pintura class="mx-2" />
-            <x-mantenimiento-aires class="mx-2" />
-            <x-mantenimiento-electrico class="mx-2" />
-            <x-orden-aseo class="mx-2" />
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-pintura class="mx-2" />
+            </div>
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-mantenimiento-aires class="mx-2" />
+            </div>
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-mantenimiento-electrico class="mx-2" />
+            </div>
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-orden-aseo class="mx-2" />
+            </div>
         </div>
         <div class="flex justify-center w-full">
-            <x-limpieza-de-banos class="mx-2" />
-            <x-limpieza-areas-comunes class="mx-2" />
-            <x-recepcion class="mx-2" />
-            <x-administracion class="mx-2" />
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-limpieza-de-banos class="mx-2" />
+            </div>
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-limpieza-areas-comunes class="mx-2" />
+            </div>
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-recepcion class="mx-2" />
+            </div>
+            <div class="transform transition-transform duration-300 hover:scale-110">
+                <x-administracion class="mx-2" />
+            </div>
         </div>
     </div>
 
     <x-footer />
-
 </x-app-layout>
-
